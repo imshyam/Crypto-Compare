1a2d6017a9896722fdc730076b7cb2a1890221f3
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-15 16:25:34 +0800

    add toolbar with menu

--- app/src/main/java/com/shapps/cryptocompare/MainActivity.kt
+++ app/src/main/java/com/shapps/cryptocompare/MainActivity.kt
@@ -5,9 +5,15 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.View
 import com.shapps.cryptocompare.Model.LiveDataContent
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.MenuInflater
+
+
 
 class MainActivity : AppCompatActivity(), DashboardFragment.OnListFragmentInteractionListener,
         SettingsFragment.OnFragmentInteractionListener, ChartsFragment.OnFragmentInteractionListener {
@@ -43,12 +49,19 @@ class MainActivity : AppCompatActivity(), DashboardFragment.OnListFragmentIntera
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_content_fragment, ChartsFragment.newInstance("Apple", "Book"))
         transaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 
     override fun onListFragmentInteraction(item: LiveDataContent.LiveData) {
