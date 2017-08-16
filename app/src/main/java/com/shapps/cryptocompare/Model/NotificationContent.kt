33db065351f1f8df204d11ce8c3f0f53537204d6
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-17 03:02:47 +0800

    add notifications fragment

---
+++ app/src/main/java/com/shapps/cryptocompare/Model/NotificationContent.kt
@@ -0,0 +1,48 @@
+package com.shapps.cryptocompare.Model
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+object NotificationContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    val ITEMS: MutableList<NotificationItem> = ArrayList<NotificationItem>()
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    private val ITEM_MAP: MutableMap<String, NotificationItem> = HashMap<String, NotificationItem>()
+
+    private val COUNT = 25
+
+    init {
+        // Add some sample items.
+        for (i in 1..COUNT) {
+            addItem(createDummyItem(i))
+        }
+    }
+
+    private fun addItem(item: NotificationItem) {
+        ITEMS.add(item)
+        ITEM_MAP.put(item.id, item)
+    }
+
+    private fun createDummyItem(position: Int): NotificationItem {
+        return NotificationItem((position).toString(), "SGD " + position, "FYB-SG", 1231.25, "buy")
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    class NotificationItem(val id: String, val currency: String, val site: String,
+                                val value: Double, val type: String)
+}
---
+++ app/src/main/java/com/shapps/cryptocompare/MyItemRecyclerViewAdapter.kt
@@ -0,0 +1,46 @@
+package com.shapps.cryptocompare
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.shapps.cryptocompare.NotificationsFragment.OnListFragmentInteractionListener
+import com.shapps.cryptocompare.Model.NotificationContent.NotificationItem
+import kotlinx.android.synthetic.main.fragment_item.view.*
+
+class MyItemRecyclerViewAdapter(private val mValues: List<NotificationItem>, private val mListener: OnListFragmentInteractionListener?) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mItem = mValues[position]
+
+        holder.bindValues(mValues[position])
+
+        holder.mView.setOnClickListener {
+            if (null != mListener) {
+                mListener!!.onListFragmentInteraction(holder.mItem as NotificationItem)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mValues.size
+    }
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        var mItem: NotificationItem? = null
+
+        fun bindValues(mValues: NotificationItem) {
+            mView.currency.text = mValues.currency
+            mView.site.text = mValues.site
+            mView.value.text = mValues.value.toString()
+            mView.type.text = mValues.type
+        }
+    }
+}
