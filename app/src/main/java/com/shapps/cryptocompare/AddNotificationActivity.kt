b599b232bd9b5cc84417b26dc6d0d20848b3cec9
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-19 19:51:19 +0800

    add notification activity

---
+++ app/src/main/java/com/shapps/cryptocompare/AddNotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.shapps.cryptocompare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNotificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notification)
+    }
+}
