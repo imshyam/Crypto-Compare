5581f3e071049a8203cb205144736c5ba267d817
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-20 00:52:11 +0800

    add create notification

--- app/src/main/java/com/shapps/cryptocompare/AddNotificationActivity.kt
+++ app/src/main/java/com/shapps/cryptocompare/AddNotificationActivity.kt
@@ -2,11 +2,69 @@ package com.shapps.cryptocompare
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_add_notification.*
+import java.util.*
 
-class AddNotificationActivity : AppCompatActivity() {
+class AddNotificationActivity : AppCompatActivity(), OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notification)
+
+        val spinnerArray = ArrayList<CharSequence>()
+        spinnerArray.add("Bitcoin")
+        spinnerArray.add("Ethereum")
+        spinnerArray.add("Litecoin")
+        val cryptoCurrencyArray = ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, spinnerArray)
+        cryptoCurrencyArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+        spinner_crypto_currency.adapter = cryptoCurrencyArray
+
+
+        val spinnerArray1 = ArrayList<String>()
+        spinnerArray1.add("SGD")
+        spinnerArray1.add("INR")
+        spinnerArray1.add("USD")
+        spinnerArray1.add("EUR")
+        val currencyArray = ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, spinnerArray1)
+        currencyArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner_currency.adapter = currencyArray
+
+
+        val spinnerArray2 = ArrayList<String>()
+        spinnerArray2.add("FYB-SG")
+        spinnerArray2.add("zebpay")
+        spinnerArray2.add("coinbase")
+        val exchangeArray = ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, spinnerArray2)
+        exchangeArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner_exchange.adapter = exchangeArray
+
+        spinner_crypto_currency.onItemSelectedListener = this
+        spinner_currency.onItemSelectedListener = this
+        spinner_exchange.onItemSelectedListener = this
+
+        price_picker.maxValue = 20
+        price_picker.minValue = 0
+        price_picker.value = 5
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, p1: View?, pos: Int, id: Long) {
+        Log.d("Selected : ", parent?.getItemAtPosition(pos).toString())
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.d("Nothing ", "Selected")
     }
 }
--- app/src/main/java/com/shapps/cryptocompare/NotificationsFragment.kt
+++ app/src/main/java/com/shapps/cryptocompare/NotificationsFragment.kt
@@ -1,6 +1,7 @@
 package com.shapps.cryptocompare
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 import com.shapps.cryptocompare.Model.NotificationContent
 import com.shapps.cryptocompare.Model.NotificationContent.NotificationItem
@@ -39,7 +41,15 @@ class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         if(noNotifications){
-            return inflater!!.inflate(R.layout.fragment_no_notification, container, false)
+            val view = inflater!!.inflate(R.layout.fragment_no_notification, container, false)
+
+            val addNotification = view.findViewById<Button>(R.id.add_notification)
+            addNotification.setOnClickListener{
+                val addNotificationAct = Intent(context, AddNotificationActivity::class.java)
+                startActivity(addNotificationAct)
+            }
+
+            return view
         }
 
         val view = inflater!!.inflate(R.layout.fragment_notification_list, container, false)
