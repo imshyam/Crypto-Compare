1ccdca0622e290dfe052e2e8c2c9bb2d311a11ca
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-27 16:05:18 +0800

    URLs

---
+++ app/src/main/java/com/shapps/cryptocompare/Networking/DetailURLs.kt
@@ -0,0 +1,12 @@
+package com.shapps.cryptocompare.Networking
+
+/**
+ * Created by shyam on 27/8/17.
+ */
+class DetailURLs {
+
+    val URL_EXCHANGES = "XXX"
+    val URL_GET_CURRENT = "XXX"
+    val URL_GET_HISTORY = "XXX"
+    val URL_CREATE_NOTIFICATION = "XXX"
+}
