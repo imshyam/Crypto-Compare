c1eba4cb7c13f617ea3a28c2ab69e41658fc7f8f
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-08-29 22:25:56 +0800

    get ethereum exchanges list

--- app/src/main/java/com/shapps/cryptocompare/SettingsFragments/ManageExchanges/Ethereum.kt
+++ app/src/main/java/com/shapps/cryptocompare/SettingsFragments/ManageExchanges/Ethereum.kt
@@ -1,11 +1,24 @@
 package com.shapps.cryptocompare.SettingsFragments.ManageExchanges
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import android.preference.SwitchPreference
+import android.util.Log
+import android.util.TypedValue
+import android.view.ContextThemeWrapper
 import android.view.MenuItem
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyLog
+import com.android.volley.toolbox.StringRequest
 import com.shapps.cryptocompare.R
 import com.shapps.cryptocompare.Activities.Settings
+import com.shapps.cryptocompare.Networking.AppController
+import com.shapps.cryptocompare.Networking.DetailURLs
 import com.shapps.cryptocompare.SettingsFragments.Main
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * Created by shyam on 26/8/17.
@@ -14,7 +27,50 @@ class Ethereum : PreferenceFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.pref_bitcoin_exchanges)
+
+
+        var prefBitcoinsScreen = preferenceManager.createPreferenceScreen(activity)
+        preferenceScreen = prefBitcoinsScreen
+
+        val themeTypedValue = TypedValue()
+        activity.theme.resolveAttribute(R.style.AppTheme, themeTypedValue, true)
+        val contextThemeWrapper = ContextThemeWrapper(activity, themeTypedValue.resourceId)
+
+        var url = DetailURLs.URL_EXCHANGES
+
+        val pDialog = ProgressDialog(activity)
+        pDialog.setMessage("Loading...")
+        pDialog.show()
+
+        val strReq = StringRequest(Request.Method.GET,
+                url, Response.Listener { response ->
+            var jsonObj = JSONObject(response);
+            var allBitcoinExchanges =  JSONObject(jsonObj.getString("Ethereum"))
+            val keys = allBitcoinExchanges.keys()
+
+            while (keys.hasNext()) {
+                val key = keys.next() as String
+                var currencywiseB =  JSONArray(allBitcoinExchanges.get(key).toString())
+//                Log.d("CurrencyWise", currencywiseB.toString())
+                (0 until currencywiseB.length())
+                        .map { JSONObject(currencywiseB.get(it).toString()) }
+                        .forEach {
+                            Log.d(key, it.getString("name"))
+                            var switchPref = SwitchPreference(contextThemeWrapper)
+                            switchPref.title = it.getString("name")
+                            switchPref.key = "pref_key_storage_bitcoin_exchanges_" + key + "_" + it.getString("name")
+                            switchPref.summary = key
+                            preferenceScreen.addPreference(switchPref)
+                        }
+            }
+            pDialog.hide()
+        }, Response.ErrorListener { error ->
+            VolleyLog.d("TAG ", "Error: " + error.message)
+            pDialog.hide()
+        })
+
+        // Adding request to request queue
+        AppController.instance?.addToRequestQueue(strReq, "APPLE", activity)
 
         val settingsAct = activity as Settings
         settingsAct.title = "Ethereum Exchanges"
