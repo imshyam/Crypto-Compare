7025b85a9f6eee2215066396e3565200a94091ac
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-10-06 03:30:35 +0800

    update save data function

--- app/src/main/java/com/shapps/cryptocompare/Model/LiveDataContent.kt
+++ app/src/main/java/com/shapps/cryptocompare/Model/LiveDataContent.kt
@@ -24,10 +24,6 @@ import android.R.id.edit
 
 object LiveDataContent {
     /**
-     * Shared Preferences File Name
-     */
-    private const val PREF_FILE = "ExchangesList"
-    /**
      * An array of sample items.
      */
     val ITEMS: MutableList<LiveData> = ArrayList()
@@ -41,7 +37,7 @@ object LiveDataContent {
         // Add some sample items.
     }
 
-    private fun addItem(item: LiveData) {
+    fun addItem(item: LiveData) {
         ITEMS.add(item)
         ITEM_MAP.put(item.id, item)
     }
@@ -49,70 +45,4 @@ object LiveDataContent {
     class LiveData(val id: String, val cryptoCurrency: String, val currency: String, val exchangeId: String, val exchangeName: String,
                    val priceBuy: String, val priceSell: String, val volume: String, val lowBuy: String, val highBuy: String,
                    val lowSell: String, val highSell: String)
-
-    fun getData(activityContext: Main, currentExchanges: String) {
-        Log.d("Load", "Ing")
-
-        val url = DetailURLs.URL_GET_CURRENT + currentExchanges
-
-        val strReq = StringRequest(Request.Method.GET,
-                url, Response.Listener { response ->
-            var currentData = JSONArray(response)
-            for(i in 0 until currentData.length()){
-                var exchangeCurrent = JSONObject(currentData.get(i).toString())
-                var cryptoCurr = exchangeCurrent.getString("crypto_curr")
-                var currency = exchangeCurrent.getString("curr")
-                var exchangeId = exchangeCurrent.getString("exchange_id")
-                var priceBuy = exchangeCurrent.getString("buy")
-                var priceSell = exchangeCurrent.getString("sell")
-                var timeInt = 1
-                var volume = "100"
-                var lowBuy = ""
-                var highBuy = ""
-                var lowSell = ""
-                var highSell = ""
-                when (timeInt) {
-                    1 -> {
-                        lowBuy = exchangeCurrent.getString("last_hour_min_buy")
-                        highBuy = exchangeCurrent.getString("last_hour_max_buy")
-                        lowSell = exchangeCurrent.getString("last_hour_min_sell")
-                        highSell = exchangeCurrent.getString("last_hour_max_sell")
-                    }
-                    2 -> {
-                        lowBuy = exchangeCurrent.getString("last_day_min_buy")
-                        highBuy = exchangeCurrent.getString("last_day_max_buy")
-                        lowSell = exchangeCurrent.getString("last_day_min_sell")
-                        highSell = exchangeCurrent.getString("last_day_max_sell")
-                    }
-                    3 -> {
-                        lowBuy = exchangeCurrent.getString("last_week_min_buy")
-                        highBuy = exchangeCurrent.getString("last_week_max_buy")
-                        lowSell = exchangeCurrent.getString("last_week_min_sell")
-                        highSell = exchangeCurrent.getString("last_week_max_sell")
-                    }
-                    4 -> {
-                        lowBuy = exchangeCurrent.getString("last_month_min_buy")
-                        highBuy = exchangeCurrent.getString("last_month_max_buy")
-                        lowSell = exchangeCurrent.getString("last_month_min_sell")
-                        highSell = exchangeCurrent.getString("last_month_max_sell")
-                    }
-                }
-
-
-                var sharedPref = activityContext.getSharedPreferences(PREF_FILE, 0)
-                var name = sharedPref.getString(exchangeId, "No Name Found")
-
-                addItem(LiveData(i.toString(), cryptoCurr, currency , exchangeId,
-                        name, priceBuy, priceSell, volume, lowBuy, highBuy, lowSell, highSell))
-            }
-            Log.d("currentData : ", currentData.toString())
-            Log.d("Load", "Done")
-        }, Response.ErrorListener { error ->
-            VolleyLog.d("TAG ", "Error: " + error.message)
-        })
-
-        // Adding request to request queue
-        AppController.instance?.addToRequestQueue(strReq, "APPLE", activityContext)
-
-    }
 }
