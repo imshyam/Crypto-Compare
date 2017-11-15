b14a74d7292f197c6b89e49e96e8b17afba303c2
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-11-16 02:44:57 +0800

    save data to db

--- app/src/main/java/com/shapps/cryptocompare/Constants/Exchanges.kt
+++ app/src/main/java/com/shapps/cryptocompare/Constants/Exchanges.kt
@@ -1,9 +1,14 @@
 package com.shapps.cryptocompare.Constants
 
 import android.util.Log
+import com.shapps.cryptocompare.Activities.Main
+import com.shapps.cryptocompare.Model.ExchangeDetailsDbHelper
 
 import org.json.JSONArray
 import org.json.JSONObject
+import android.content.ContentValues
+import com.shapps.cryptocompare.Model.ExchangeDetailsSchema.ExchangesDetailsEntry.*
+
 
 /**
  * Created by shyam on 28/10/17.
@@ -14,22 +19,30 @@ class Exchanges {
 
     companion object {
 
-        fun saveData(json: String?) {
+        fun saveData(json: String?, context: Main?): Boolean {
             Log.d("Ex : ", json)
             try {
                 var jsonArr = JSONArray(json)
-
+                val mDbHelper = ExchangeDetailsDbHelper(context)
+                val db = mDbHelper.writableDatabase
                 (0 until jsonArr.length())
                         .map { JSONObject(jsonArr.get(it).toString()) }
                         .forEach {
-                            if (it.getString("crypto_currency").equals("Bitcoin")) {
-                                var currency = it.getString("currency")
-                                Log.d("Exchange", it.getString("name"))
-                            }
+                            // Create a new map of values, where column names are the keys
+                            val values = ContentValues()
+                            values.put(COLUMN_NAME_ID, it.getString("id"))
+                            values.put(COLUMN_NAME_CRYPTO_CURRENCY, it.getString("crypto_currency"))
+                            values.put(COLUMN_NAME_CURRENCY, it.getString("currency"))
+                            values.put(COLUMN_NAME_EX_NAME, it.getString("name"))
+                            values.put(COLUMN_NAME_ACTIVE, 0)
+
+                            db.insert(TABLE_NAME, null, values)
                         }
+                return true
 
             }
             catch (ex: Exception){
+                return  false
             }
         }
     }
