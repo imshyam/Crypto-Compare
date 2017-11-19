688d49054fe6af3f1db709c9f85ae123fe619fc3
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-11-20 02:29:35 +0800

    update imports

--- app/src/main/java/com/shapps/cryptocompare/Model/LiveDataContent.kt
+++ app/src/main/java/com/shapps/cryptocompare/Model/LiveDataContent.kt
@@ -1,20 +1,7 @@
 package com.shapps.cryptocompare.Model
 
-import android.content.SharedPreferences
-import android.preference.SwitchPreference
-import android.util.Log
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.VolleyLog
-import com.android.volley.toolbox.StringRequest
-import com.shapps.cryptocompare.Activities.Main
-import com.shapps.cryptocompare.Networking.AppController
-import com.shapps.cryptocompare.Networking.DetailURLs
-import org.json.JSONArray
-import org.json.JSONObject
 import java.util.ArrayList
 import java.util.HashMap
-import android.R.id.edit
 
 
 
