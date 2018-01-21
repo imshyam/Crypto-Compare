662bac3a2f2911feca39b2275d23c1220d88bc3d
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-01-22 02:31:15 +0800

    show and update chart on click

--- app/src/main/java/com/shapps/cryptocompare/Fragments/Charts.kt
+++ app/src/main/java/com/shapps/cryptocompare/Fragments/Charts.kt
@@ -33,6 +33,7 @@ import com.shapps.cryptocompare.Model.ExchangeDetailsSchema.ExchangesDetailsEntr
 import com.shapps.cryptocompare.Model.LiveDataContent
 import com.shapps.cryptocompare.Networking.AppController
 import com.shapps.cryptocompare.Networking.DetailURLs
+import com.shapps.cryptocompare.Networking.History
 import com.shapps.cryptocompare.R
 import org.json.JSONArray
 import org.json.JSONObject
@@ -49,7 +50,7 @@ class Charts : Fragment(), View.OnClickListener {
 
     private var mListener: OnFragmentInteractionListener? = null
 
-    private var lineChart: LineChart? = null
+    private lateinit var lineChart: LineChart
 
     private lateinit var btn1Hour: Button
     private lateinit var btn1Day: Button
@@ -63,8 +64,8 @@ class Charts : Fragment(), View.OnClickListener {
 
         val view_main: View = inflater!!.inflate(R.layout.fragment_charts, container, false)
 
-
-//        insertDataList(view_main)
+        lineChart = view_main.findViewById(R.id.price_chart)
+        History.draw("1", "Fyb-Sg", "hours=1", context, lineChart, "12345", "12345")
 
         val spinner = view_main?.findViewById<Spinner>(R.id.currency_spinner)
         var adapter = ArrayAdapter.createFromResource(activity, R.array.currency_list, android.R.layout.simple_spinner_item)
@@ -92,22 +93,27 @@ class Charts : Fragment(), View.OnClickListener {
             R.id.period_1_hour -> {
                 var x = v as Button
                 updateStyle(x)
+                History.draw("1", "Fyb-Sg", "hours=1", context, lineChart, "12345", "12345")
             }
             R.id.period_1_day -> {
                 var x = v as Button
                 updateStyle(x)
+                History.draw("1", "Fyb-Sg", "days=1", context, lineChart, "12345", "12345")
             }
             R.id.period_1_week -> {
                 var x = v as Button
                 updateStyle(x)
+                History.draw("1", "Fyb-Sg", "days=7", context, lineChart, "12345", "12345")
             }
             R.id.period_1_month -> {
                 var x = v as Button
                 updateStyle(x)
+                History.draw("1", "Fyb-Sg", "days=30", context, lineChart, "12345", "12345")
             }
             R.id.period_all -> {
                 var x = v as Button
                 updateStyle(x)
+                History.draw("1", "Fyb-Sg", "days=100", context, lineChart, "12345", "12345")
             }
             else -> {
 
@@ -133,117 +139,6 @@ class Charts : Fragment(), View.OnClickListener {
         btnAll.background = ContextCompat.getDrawable(context, R.drawable.time_period_rounded)
         btnAll.setTextColor(Color.BLACK)
     }
-
-//    private fun insertDataList(view_main: View) {
-//
-//        var id_name_map: HashMap<Int, String> = hashMapOf()
-//        var getCurrentExchanges = ""
-//
-//        val mDbHelper = ExchangeDetailsDbHelper(activity)
-//
-//        val db = mDbHelper.readableDatabase
-//
-//        val projection = arrayOf(
-//                COLUMN_NAME_ID,
-//                COLUMN_NAME_EX_NAME,
-//                COLUMN_NAME_CRYPTO_CURRENCY,
-//                COLUMN_NAME_CURRENCY)
-//
-//// Filter results WHERE "title" = 'My Title'
-//        val selection = ExchangeDetailsSchema.ExchangesDetailsEntry.COLUMN_NAME_ACTIVE + " = ?"
-//        val selectionArgs = arrayOf("1")
-//
-//// How you want the results sorted in the resulting Cursor
-//        val sortOrder = ExchangeDetailsSchema.ExchangesDetailsEntry.COLUMN_NAME_ID + " ASC"
-//
-//        val cursor = db.query(
-//                ExchangeDetailsSchema.ExchangesDetailsEntry.TABLE_NAME, // The table to query
-//                projection, // The columns to return
-//                selection, // The columns for the WHERE clause
-//                selectionArgs, // don't group the rows
-//                null, null, // don't filter by row groups
-//                sortOrder                                 // The sort order
-//        )
-//        while (cursor.moveToNext()) {
-//            id_name_map.put(cursor.getInt(0), cursor.getString(1))
-//            getCurrentExchanges += cursor.getInt(0).toString() + ","
-//        }
-//        cursor.close()
-//
-//        if(getCurrentExchanges.length > 1)
-//            getCurrentExchanges = getCurrentExchanges.substring(0, getCurrentExchanges.length-1)
-//
-//        Log.d("Tmp " , getCurrentExchanges)
-//
-//
-//        var pDialog = ProgressDialog(activity)
-//        pDialog.setMessage("Loading...")
-//        pDialog.show()
-//
-//        val url = DetailURLs.URL_GET_HISTORY + getCurrentExchanges + "&hours=1"
-//
-//        val map: HashMap<Int, MutableList<HashMap<String, MutableList<Float>>>> = hashMapOf()
-//        val strReq = StringRequest(Request.Method.GET,
-//                url, Response.Listener { response ->
-//            var historyData = JSONArray(response)
-//            LiveDataContent.dumpData()
-//            for(i in 0 until historyData.length()){
-//                var exchangeCurrent = JSONObject(historyData.get(i).toString())
-//                var cryptoCurr = exchangeCurrent.getString("crypto_curr")
-//                var currency = exchangeCurrent.getString("curr")
-//                var exchangeId = exchangeCurrent.getString("exchange_id")
-//                var priceBuy = exchangeCurrent.getString("buy")
-//                var priceSell = exchangeCurrent.getString("sell")
-//                var date_time = exchangeCurrent.getString("date_time")
-//
-//                var buyPrice = priceBuy.toFloat()
-//                var sellPrice = priceSell.toFloat()
-//                if(map.containsKey(exchangeId.toInt())){
-//                    map[exchangeId.toInt()]!![0]["buy"]!!.add(buyPrice)
-//                    map[exchangeId.toInt()]!![1]["sell"]!!.add(sellPrice)
-//                }
-//                else{
-//                    var buyInit = hashMapOf<String, MutableList<Float>>("buy" to mutableListOf())
-//                    map.put(exchangeId.toInt(), mutableListOf())
-//                    var sellInit = hashMapOf<String, MutableList<Float>>("sell" to mutableListOf())
-//                    map.put(exchangeId.toInt(), mutableListOf())
-//                    map[exchangeId.toInt()]!!.add(buyInit)
-//                    map[exchangeId.toInt()]!!.add(sellInit)
-//                }
-//
-//            }
-//            pDialog.hide()
-//            pDialog.dismiss()
-//            var entries = ArrayList<Entry>()
-//            (0 until map[4]!![0]["buy"]!!.size).mapTo(entries) { Entry(it.toFloat(), map[4]!![0]["buy"]!![it]) }
-//            var lds = LineDataSet(entries, "FYB-SG")
-//            lds.color = Color.parseColor("#003838")
-//            lds.valueTextColor = Color.parseColor("#bbbbbb")
-//
-//            var entries1 = ArrayList<Entry>()
-//            (0 until map[4]!![1]["sell"]!!.size).mapTo(entries1) { Entry(it.toFloat(), map[4]!![1]["sell"]!![it]) }
-//            var lds1 = LineDataSet(entries1, "FYB-SG-Sell")
-//            lds1.color = Color.parseColor("#01B6AD")
-//            lds1.valueTextColor = Color.parseColor("#0A4958")
-//
-//            var list: List<ILineDataSet> = listOf(lds, lds1)
-//
-//            // Implement this
-//            lineChart = view_main!!.findViewById<View>(R.id.price_chart) as LineChart
-//            lineChart!!.data = LineData(list)
-//            lineChart!!.invalidate()
-//
-//
-//        }, Response.ErrorListener { error ->
-//            VolleyLog.d("TAG ", "Error: " + error.message)
-//            pDialog.hide()
-//            pDialog.dismiss()
-//            Toast.makeText(activity, "Network Error", Toast.LENGTH_SHORT).show()
-//        })
-//
-//        // Adding request to request queue
-//        AppController.instance?.addToRequestQueue(strReq, "APPLE", activity)
-//    }
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
